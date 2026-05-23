2a82de081be6b453951f7de2bb7ac4386add6db8
bflaggs <flaggsben@gmail.com>
2026-05-23 15:26:59 -0400

    Refactor code to remove observatory dependence on certain functions

--- Generalized_MultiMassML_FOM_ScriptVersion.py
+++ Generalized_MultiMassML_FOM_ScriptVersion.py
@@ -12,6 +12,8 @@ import matplotlib.gridspec as gridspec
 from matplotlib.ticker import MultipleLocator
 from matplotlib import rc, rcParams
 
+import os
+
 rc("font", size=18.0)
 rcParams["font.family"] = "serif"
 rcParams["mathtext.fontset"] = "dejavuserif"
@@ -28,15 +30,12 @@ from sklearn.model_selection import RandomizedSearchCV
 #### In notebook, each function is it's own cell ####
 
 # Function to read in the data and do some renaming of columns
-def ReadData(observatory):
-    if observatory == "IceCube":
-        url = "https://raw.githubusercontent.com/bflaggs/CRMassMultivariateML/main/data/energy-corrected/IceCube_Condensed_AllParticles_AllEnergiesAndZeniths_EnergyCorrected.txt"
-    elif observatory == "Auger":
-        url = "https://raw.githubusercontent.com/bflaggs/CRMassMultivariateML/main/data/energy-corrected/Auger_Condensed_AllParticles_AllEnergiesAndZeniths_EnergyCorrected.txt"
+def ReadData(filename):
+    # Check file exists
+    if os.path.exists(filename):
+        df = pd.read_csv(filename, sep=" ")
     else:
-        raise ValueError("Not possible for this observatory. Only 'IceCube' and 'Auger' are valid inputs.")
-
-    df = pd.read_csv(url, sep=" ")
+        raise ValueError(f"File {filename} does not exist. Check path.")
 
     # Rename some columns to make things easier for future...
     df.columns = ["ParticleID", "EnergyGeV", "zenith", "nEM_Xmax", "nEM_Obslev", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "nMu800m", "R_eMu800m", "Xmax", "SigmaXmax", "R", "SigmaR", "L", "SigmaL"]
@@ -52,7 +51,7 @@ def ApplyFisherDataCuts(dataset, ProtonIron=False, HeliumOxygen=False, ProtonHel
     # Drop bad data, same cuts as my Fisher analysis
     dfCuts = dataset[(dataset.nMuHighE != -np.inf) &
                     (dataset.nMu800m != -np.inf) &
-                    (dataset.R_eMu800m != -np.inf) &
+                    (dataset.R_eMu800m != -np.inf) &  # Should be same as cutting all events w/ nEM800m <= 0.0
                     (dataset.SigmaXmax != np.inf) &
                     (dataset.SigmaR != np.inf) &
                     (dataset.SigmaL != np.inf) &
@@ -60,7 +59,7 @@ def ApplyFisherDataCuts(dataset, ProtonIron=False, HeliumOxygen=False, ProtonHel
                     (dataset.SigmaR < 0.05) &
                     (dataset.SigmaL < 5.0) &
                     (dataset.R > 0.0) &
-                    (dataset.L < 350.0)]
+                    (dataset.Xmax < 1500.0)]
 
     if ProtonIron == True:
         # Cut He and O
@@ -111,32 +110,22 @@ def ApplyZenithCut(dataset, minZen=40.0, maxZen=60.0):
 
 
 # Function to apply energy bin cuts, used to split data in energy so that multiple models can be trained
-def BinByEnergy(dataset, minLgE=16.0, maxLgE=18.5):
+def BinByEnergy(dataset, minLgE=16.0, maxLgE=20.5):
     dfEnergyBinned = dataset[(np.log10(dataset.EnergyGeV * 1e+9) >= minLgE) &
                             (np.log10(dataset.EnergyGeV * 1e+9) <= maxLgE)]
     return dfEnergyBinned
 
 
 # Function to drop columns not used in Fisher analysis and separate array of particle labels (goal) from data
-def GetDataForGBDT(dataset, observatory, dropZenith=False):
-    if observatory == "IceCube":
-        if dropZenith == True:
-            dfGBDT = dataset.drop(columns=["EnergyGeV", "zenith", "nEM_Xmax", "nEM800m_NOTCORRECTED", "SigmaXmax", "SigmaR", "SigmaL"])
-            smearLevels = [0.1, 0.07, 0.12, 0.1, 0.14, 20.0, 0.05, 5.0]
-        else:
-            dfGBDT = dataset.drop(columns=["EnergyGeV", "nEM_Xmax", "nEM800m_NOTCORRECTED", "SigmaXmax", "SigmaR", "SigmaL"])
-            smearLevels = [2.0 * np.pi / 180.0, 0.1, 0.07, 0.12, 0.1, 0.14, 20.0, 0.05, 5.0]
-
-    elif observatory == "Auger":
-        if dropZenith == True:
-            dfGBDT = dataset.drop(columns=["EnergyGeV", "zenith", "nEM_Xmax", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "SigmaXmax", "SigmaR", "SigmaL"])
-            smearLevels = [0.1, 0.1, 0.14, 20.0, 0.05, 5.0]
-        else:
-            dfGBDT = dataset.drop(columns=["EnergyGeV", "nEM_Xmax", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "SigmaXmax", "SigmaR", "SigmaL"])
-            smearLevels = [2.0 * np.pi / 180.0, 0.1, 0.1, 0.14, 20.0, 0.05, 5.0]
-
+def GetDataForGBDT(dataset, dropZenith=False):
+    if dropZenith == True:
+        dfGBDT = dataset.drop(columns=["EnergyGeV", "zenith", "nEM_Xmax", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "SigmaXmax", "SigmaR", "SigmaL"])
+        smearLevels = [0.1, 0.1, 0.14, 20.0, 0.05, 5.0]
     else:
-        raise ValueError("Not possible for this observatory. Only 'IceCube' and 'Auger' are valid inputs.")
+        dfGBDT = dataset.drop(columns=["EnergyGeV", "nEM_Xmax", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "SigmaXmax", "SigmaR", "SigmaL"])
+        smearLevels = [2.0 * np.pi / 180.0, 0.1, 0.1, 0.14, 20.0, 0.05, 5.0]
+
+    print("WARNING: To keep the high-energy muon number this function should be updated and the observable studied in more detail.")
 
     # Define another array which contains the labels of the particles
     particleIDs = np.array(dfGBDT["ParticleID"])
@@ -184,10 +173,14 @@ def GetFOM(GBDT, testData, testIDs, plotLabels, smearValues=False, smearArray=No
 
     reverseSorted_pop2 = np.sort(pop2_gbdt_pred)[::-1]
     pop2_5perOverlap = reverseSorted_pop2[:int(len(pop2_gbdt_pred)* 0.05)]
-    pop1_5perContamination = pop1_gbdt_pred[np.nonzero(pop1_gbdt_pred > pop2_5perOverlap[-1])]
+    pop1_5perContamination = pop1_gbdt_pred[np.nonzero(pop1_gbdt_pred >= pop2_5perOverlap[-1])]
 
     pop2_1perOverlap = reverseSorted_pop2[:int(len(pop2_gbdt_pred)* 0.01)]
-    pop1_1perContamination = pop1_gbdt_pred[np.nonzero(pop1_gbdt_pred > pop2_1perOverlap[-1])]
+    pop1_1perContamination = pop1_gbdt_pred[np.nonzero(pop1_gbdt_pred >= pop2_1perOverlap[-1])]
+
+    # Could update these to quantiles like so:
+    #pop2_thresh5 = np.quantile(pop2_gbdt_pred, 0.95)
+    #pop1_5perContamination = pop1_gbdt_pred[pop1_gbdt_pred >= pop2_thresh5]
 
     frac_sep_pop1_5perContamination = len(pop1_5perContamination) / len(pop1_gbdt_pred)
     frac_sep_pop1_1perContamination = len(pop1_1perContamination) / len(pop1_gbdt_pred)
@@ -198,6 +191,8 @@ def GetFOM(GBDT, testData, testIDs, plotLabels, smearValues=False, smearArray=No
     std_pop2 = np.std(pop2_gbdt_pred)
 
     fom_gbdt = np.abs(mean_pop1 - mean_pop2) / np.sqrt(std_pop1**2 + std_pop2**2)
+
+    # NOTE: Should also calculated an AUC score and directly compare the GBDT score to the linear discriminant score
 
     if printOutput == True:
         if smearValues == True:
@@ -229,7 +224,7 @@ def GetFOM(GBDT, testData, testIDs, plotLabels, smearValues=False, smearArray=No
 
 
 # Function to make the output histogram
-def MakeHistogram(observatory, pop1_pred, pop2_pred, fom, cont_5per, cont_1per, plotLabels, plotColors, lgEbins=[16.0,18.5], smearValues=False, smearArray=None):
+def MakeHistogram(observatory, hadronic_model, pop1_pred, pop2_pred, fom, cont_5per, cont_1per, plotLabels, plotColors, lgEbins=[16.0,20.5], smearValues=False, smearArray=None):
 
     plt.figure(figsize=(18.0 / 2.54, 15.0 / 2.54))
     n2, bins2, patches2 = plt.hist(pop2_pred, bins=100, histtype="step", color=plotColors[1], label=plotLabels[1])
@@ -253,19 +248,20 @@ def MakeHistogram(observatory, pop1_pred, pop2_pred, fom, cont_5per, cont_1per, 
     plt.vlines(pop2_1perOverlap[-1], 0, (max_counts / 3), colors="black", linestyle="dotted", label="1% Contamination")
     plt.text(0.1, (max_counts / 2), f"Frac. Sep. {sep_pop_string} = {cont_5per:.3f} @ 5% cont.", fontsize=12)
     plt.text(0.1, (max_counts / 2.2), f"Frac. Sep. {sep_pop_string} = {cont_1per:.3f} @ 1% cont.", fontsize=12)
-    plt.text(0.05, (max_counts / 1.5), f"{observatory} (FOM = {fom:.2f})")
+    plt.text(0.05, (max_counts / 1.5), f"{hadronic_model} (FOM = {fom:.2f})")
+    plt.text(0.05, (max_counts / 1.5) - 0.06, f"{observatory}")
     plt.legend(loc="best", fontsize=14)
     if smearValues == True:
         plt.xlabel("GBT Regressor Output (Smeared)")
     else:
         plt.xlabel("GBT Regressor Output (True)")
     plt.ylabel("Counts")
-    plt.title(f"lg(E / eV) = {lgEbins[0]:.1f}-{lgEbins[1]:.1f}")
+    plt.title(rf"log$_{{10}}$(E / eV) = {lgEbins[0]:.1f}-{lgEbins[1]:.1f}")
     plt.show()
 
 
 # Setup another function which will be used to initialize the GBDT w/ a keyword option for setting up a smeared GBDT or not.
-def SetupGBDT(og_df, observatory, primaries, smearedGBDT=False, analyzeModel=True, analyzeForSmearedTestData=False):
+def SetupGBDT(og_df, primaries, smearedGBDT=False, analyzeModel=True, analyzeForSmearedTestData=False):
 
     print(f"Model trained to separate {primaries[0]} from {primaries[1]}.")
 
@@ -279,8 +275,8 @@ def SetupGBDT(og_df, observatory, primaries, smearedGBDT=False, analyzeModel=Tru
     df_train = pd.DataFrame(trainData, columns=list(og_df.columns))
     df_test = pd.DataFrame(testData, columns=list(og_df.columns))
 
-    df_train, train_obj, smearLevels = GetDataForGBDT(df_train, observatory, dropZenith=True)
-    df_test, test_obj, smearLevels = GetDataForGBDT(df_test, observatory, dropZenith=True)
+    df_train, train_obj, smearLevels = GetDataForGBDT(df_train, dropZenith=True)
+    df_test, test_obj, smearLevels = GetDataForGBDT(df_test, dropZenith=True)
 
     if (trainObjectives != train_obj).any() or (testObjectives != test_obj).any():
         raise ValueError("Inconsistent objectives for train/test sets in train-test splitting and data cuts phases.")
@@ -297,7 +293,7 @@ def SetupGBDT(og_df, observatory, primaries, smearedGBDT=False, analyzeModel=Tru
     gbt_model.fit(train_array, trainObjectives)
 
     if analyzeModel == True:
-        AnalyzeGBDT(observatory, gbt_model, train_array, trainObjectives, pd.DataFrame(testData, columns=list(og_df.columns)), smearedModel=smearedGBDT, smearTestData=analyzeForSmearedTestData)
+        AnalyzeGBDT(gbt_model, train_array, trainObjectives, pd.DataFrame(testData, columns=list(og_df.columns)), smearedModel=smearedGBDT, smearTestData=analyzeForSmearedTestData)
 
     if smearedGBDT == True:
         print("\n")
@@ -307,9 +303,9 @@ def SetupGBDT(og_df, observatory, primaries, smearedGBDT=False, analyzeModel=Tru
 
 
 # Function to analyze the GBDT, i.e. output model scores and list feature importances
-def AnalyzeGBDT(observatory, trained_model, trainingData, trainingObjectives, original_testData_df, smearedModel=False, smearTestData=False):
+def AnalyzeGBDT(trained_model, trainingData, trainingObjectives, original_testData_df, smearedModel=False, smearTestData=False):
 
-    df_testdata, testingObjectives, smearLevels = GetDataForGBDT(original_testData_df, observatory, dropZenith=True)
+    df_testdata, testingObjectives, smearLevels = GetDataForGBDT(original_testData_df, dropZenith=True)
     testingData = df_testdata.to_numpy()
 
     if smearTestData == True:
@@ -339,7 +335,7 @@ def AnalyzeGBDT(observatory, trained_model, trainingData, trainingObjectives, or
 
 
 # Function to calculate a list of FOM values in different energy bins based on the energy binned test data
-def FOMComparisonToFisher(observatory, trained_gbt, df_test_data, lgEBinEdges, plotLabels, plotColors, smearing=False, smearLevels=None, printVals=False, makeHistograms=False):
+def FOMComparisonToFisher(observatory, hadronic_model, trained_gbt, df_test_data, lgEBinEdges, plotLabels, plotColors, smearing=False, smearLevels=None, printVals=False, makeHistograms=False):
     FOM_list = []
     Contamination_5per = []
     Contamination_1per = []
@@ -351,20 +347,18 @@ def FOMComparisonToFisher(observatory, trained_gbt, df_test_data, lgEBinEdges, p
                                         (np.log10(df_test_data.EnergyGeV * 1e+9) <= lgEBinEdges[i])]
         lgEBinned_test_IDs = np.array(lgEBinned_test_data["ParticleID"])
 
-        if observatory == "IceCube":
-            lgEBinned_test_data = lgEBinned_test_data.drop(columns=["ParticleID", "EnergyGeV", "zenith", "nEM_Xmax", "nEM800m_NOTCORRECTED", "SigmaXmax", "SigmaR", "SigmaL"])
-        elif observatory == "Auger":
+        if observatory in ["Auger"] and hadronic_model in ["Sibyll 2.3e", "EPOS LHC-R", "QGSJETIII-01"]:
             lgEBinned_test_data = lgEBinned_test_data.drop(columns=["ParticleID", "EnergyGeV", "zenith", "nEM_Xmax", "nEM800m_NOTCORRECTED", "nMuHighE", "R_eMuHighE", "SigmaXmax", "SigmaR", "SigmaL"])
         else:
-            raise ValueError("Observatory must be set to 'IceCube' or 'Auger'.")
+            raise ValueError("Unstudied choices for 'observatory' and 'hadronic_model' combination.")
 
         if printVals == True:
-            print(f"For test data between lgE = {lgEBinEdges[i-1]:.1f} - {lgEBinEdges[i]:.1f} ({len(lgEBinned_test_IDs)} total events)")
+            print(f"For test data between log10(E / eV) = {lgEBinEdges[i-1]:.1f} - {lgEBinEdges[i]:.1f} ({len(lgEBinned_test_IDs)} total events)")
 
         fom, cont_5per, cont_1per, pop1_pred, pop2_pred = GetFOM(trained_gbt, lgEBinned_test_data.values, lgEBinned_test_IDs, plotLabels, smearValues=smearing, smearArray=smearLevels, printOutput=printVals)
 
         if makeHistograms == True:
-            MakeHistogram(observatory, pop1_pred, pop2_pred, fom, cont_5per, cont_1per, plotLabels, plotColors, lgEbins=[lgEBinEdges[i-1],lgEBinEdges[i]], smearValues=smearing, smearArray=smearLevels)
+            MakeHistogram(observatory, hadronic_model, pop1_pred, pop2_pred, fom, cont_5per, cont_1per, plotLabels, plotColors, lgEbins=[lgEBinEdges[i-1],lgEBinEdges[i]], smearValues=smearing, smearArray=smearLevels)
 
         FOM_list.append(fom)
         Contamination_5per.append(cont_5per)
@@ -375,17 +369,17 @@ def FOMComparisonToFisher(observatory, trained_gbt, df_test_data, lgEBinEdges, p
 
 
 # Function that does whole analysis that take the observatory, energy bins, and primary type as input
-def PerformGBTAnalysis(observatory, lgEbinsFOM, pFe=False, HeO=False, pHe=False, smear=False, verbose=False):
-    dataframe = ReadData(observatory)
+def PerformGBTAnalysis(filename, observatory, hadronic_model, lgEbinsFOM, pFe=False, HeO=False, pHe=False, smear=False, verbose=False):
+    dataframe = ReadData(filename)
     dataframe, plotLabels, plotColors = ApplyFisherDataCuts(dataframe, ProtonIron=pFe, HeliumOxygen=HeO, ProtonHelium=pHe)
     print("\n")
 
     dataframe_zenCut = ApplyZenithCut(dataframe, minZen=40., maxZen=60.) # Apply zenith cut same as Fisher analysis
-    dataframe_ECut = BinByEnergy(dataframe_zenCut, minLgE=16.0, maxLgE=18.5) # Apply energy cut same as Fisher analysis
+    dataframe_ECut = BinByEnergy(dataframe_zenCut, minLgE=16.0, maxLgE=20.5) # Apply energy cut same as Fisher analysis
 
     gbtReg, df_train, df_test, smearLevels, _junkarray, _junkobjectives = SetupGBDT(dataframe_ECut, observatory, plotLabels, smearedGBDT=smear, analyzeModel=True, analyzeForSmearedTestData=True)
 
-    FOMs, contam_5per, contam_1per, events_per_bin = FOMComparisonToFisher(observatory, gbtReg, df_test, lgEbinsFOM, plotLabels, plotColors,
+    FOMs, contam_5per, contam_1per, events_per_bin = FOMComparisonToFisher(observatory, hadronic_model, gbtReg, df_test, lgEbinsFOM, plotLabels, plotColors,
                                                                             smearing=smear, smearLevels=smearLevels, printVals=verbose, makeHistograms=True)
 
     return FOMs, contam_5per, contam_1per, events_per_bin
@@ -406,7 +400,10 @@ def PrintGBTResults(lgEbins, foms, cut_5per, cut_1per, num_events):
     print("lgE Bin (Center),   FOM,                5% Contamination,   1% Contamination,   # Events")
     [print("{:<20.1f} {:<20.2f} {:<20.3f} {:<20.3f} {:<20}".format(*bin_output)) for bin_output in eng_binned_output]
 
-lgEBinEdges = [16.0, 16.2, 16.4, 16.6, 16.8, 17.0, 17.2, 17.4, 17.6, 17.8, 18.0, 18.2, 18.4]
+#lgEBinEdges = [16.0, 16.2, 16.4, 16.6, 16.8, 17.0, 17.2, 17.4, 17.6, 17.8, 18.0, 18.2, 18.4]
+lgEBinEdges = [16.0, 16.2, 16.4, 16.6, 16.8, 17.0, 17.2, 17.4, 17.6, 17.8, 18.0, 18.2, 18.4,
+               18.6, 18.8, 19.0, 19.2, 19.4, 19.6, 19.8, 20.0, 20.2, 20.4]
+
 
 """# IceCube (p Fe Separation)"""
 
